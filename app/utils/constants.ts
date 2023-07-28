af2f8dfa0428236369a02c317b4a873a81e462c2
Étienne S <sauvage-etienne@hotmail.com>
2023-07-28 13:37:02 +0200

    Fix navigator undefined error due to SSR

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,6 +1,8 @@
 export const APP_VERSION = '2.0';
 
 export function userOnMobile(): boolean {
+  if (typeof navigator === 'undefined') return false;
+
   const userAgent = navigator.userAgent.toLowerCase();
   return /iphone|ipod|ipad|android/.test(userAgent);
 }
